56f04d6f03d76150d319cf96c97dc3889e7388f6
Nader <nsyd1582@gmail.com>
2023-09-26 23:50:44 +0300

    create colors and styles files

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const magnoliaWhite = Color(0xfffff9ff);
+  static const columbiaBlue = Color(0xffb3fcfc);
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff030303);
+  static const jumbo = Color(0xff828280);
+  static const snow = Color(0xfffcfcfc);
+  static const blackRussian = Color(0xff181725);
+  static const grey = Color(0xff7c7c7c);
+  static const darkGrey = Color(0xffb1b1b1);
+  static const oceanGreen = Color(0xff53b175);
+  static const timberGreen = Color(0xff3e423f);
+  static const blackCurrant = Color(0xff181619);
+}
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -4,4 +4,20 @@ import 'package:nectar/core/utils/colors.dart';
 abstract class Styles {
   static TextStyle style14 = const TextStyle(
       fontSize: 14, fontWeight: FontWeight.w500, color: AppColors.grey);
+  static TextStyle style16 = const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: AppColors.magnoliaWhite);
+  static TextStyle style18 = const TextStyle(
+      fontSize: 18, fontWeight: FontWeight.w600, color: AppColors.blackRussian);
+  static TextStyle style20 = const TextStyle(
+      fontSize: 20, fontWeight: FontWeight.w700, color: AppColors.timberGreen);
+  static TextStyle style24 = const TextStyle(
+      fontSize: 24, fontWeight: FontWeight.w600, color: AppColors.blackRussian);
+  static TextStyle style26 = const TextStyle(
+      fontSize: 26, fontWeight: FontWeight.w600, color: AppColors.black);
+  static TextStyle style28 = const TextStyle(
+      fontSize: 28, fontWeight: FontWeight.w600, color: AppColors.blackRussian);
+  static TextStyle style48 = const TextStyle(
+      fontSize: 48, fontWeight: FontWeight.w600, color: AppColors.white);
 }
