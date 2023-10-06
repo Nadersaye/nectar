c65053bdd25d1904d4d3aefd903002ec1dbf55d7
Nader <nsyd1582@gmail.com>
2023-10-07 00:41:28 +0300

    create Listview of checkout's body

--- lib/features/home/presentation/views/widgets/cart widgets/cart_view_body.dart
+++ lib/features/home/presentation/views/widgets/cart widgets/cart_view_body.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:nectar/core/utils/colors.dart';
 import 'package:nectar/core/utils/styles.dart';
 import 'package:nectar/core/widgets/custom_button.dart';
-import 'package:nectar/core/widgets/custom_divider.dart';
 import '../../../../data/models/cart_item_model.dart';
 import '../../../../data/models/expansion_tile_model.dart';
 import 'animated_dialog.dart';
 import 'checkout_appbar.dart';
+import 'checkout_listview.dart';
 import 'custom_cart_listview.dart';
 import 'custom_terms_text.dart';
 
@@ -166,36 +166,5 @@ class CartActionButton extends StatelessWidget {
         ),
       ),
     ]);
-  }
-}
-
-class CheckoutListView extends StatelessWidget {
-  const CheckoutListView({
-    super.key,
-    required this.expansionItems,
-  });
-  final List<ExpansionTileModel> expansionItems;
-  @override
-  Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: ExpansionTile(
-            tilePadding: EdgeInsets.zero,
-            title: Text(
-              expansionItems[index].title,
-              style: Styles.styleBlackRussian18.copyWith(color: AppColors.grey),
-            ),
-            trailing: Text(expansionItems[index].trailing,
-                style: Styles.styleBlackRussian18),
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return const CustomDivider();
-      },
-      itemCount: expansionItems.length,
-    );
   }
 }
