f9c3815f24d405fe4062f511606a27c77ec0ae25
Nader <nsyd1582@gmail.com>
2023-10-07 01:52:32 +0300

    create checkout body

--- lib/features/home/presentation/views/widgets/cart widgets/cart_view_body.dart
+++ lib/features/home/presentation/views/widgets/cart widgets/cart_view_body.dart
@@ -6,11 +6,9 @@ import 'package:nectar/core/widgets/custom_button.dart';
 import '../../../../data/models/cart_item_model.dart';
 import '../../../../data/models/expansion_tile_model.dart';
 import 'animated_dialog.dart';
-import 'checkout_appbar.dart';
-import 'checkout_listview.dart';
+import 'checkout_body.dart';
 import 'custom_cart_listview.dart';
 import 'custom_checkout_price.dart';
-import 'custom_terms_text.dart';
 
 class CartViewBody extends StatefulWidget {
   const CartViewBody({super.key});
@@ -144,42 +142,5 @@ class CartActionButton extends StatelessWidget {
         child: CustomCheckoutPriceText(),
       ),
     ]);
-  }
-}
-
-class CheckoutBody extends StatelessWidget {
-  const CheckoutBody({
-    super.key,
-    required this.expansionItems,
-    required this.onTap,
-  });
-
-  final List<ExpansionTileModel> expansionItems;
-  final Function() onTap;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const CheckOutAppBar(),
-        const Divider(
-          height: 1,
-          color: AppColors.lightGray,
-        ),
-        Expanded(
-          child: CheckoutListView(
-            expansionItems: expansionItems,
-          ),
-        ),
-        const CustomTermsText(),
-        Padding(
-          padding: const EdgeInsets.all(25),
-          child: CustomActionButton(
-            buttonText: 'Place Order',
-            onTap: onTap,
-          ),
-        ),
-      ],
-    );
   }
 }
