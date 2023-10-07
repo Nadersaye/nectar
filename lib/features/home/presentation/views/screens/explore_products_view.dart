2e8c3fd0f41ce9bae37c267ab13fe6a7dcad1658
Nader <nsyd1582@gmail.com>
2023-10-07 03:22:39 +0300

    create ExploreProductsViewBody

--- lib/features/home/presentation/views/screens/explore_products_view.dart
+++ lib/features/home/presentation/views/screens/explore_products_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../widgets/explore products widgets/explore_products_view_body.dart';
 
 class ExploreProductsView extends StatelessWidget {
   const ExploreProductsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const ExploreProductsViewBody();
   }
 }
---
+++ lib/features/home/presentation/views/widgets/explore products widgets/explore_products_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExploreProductsViewBody extends StatelessWidget {
+  const ExploreProductsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [],
+    );
+  }
+}
