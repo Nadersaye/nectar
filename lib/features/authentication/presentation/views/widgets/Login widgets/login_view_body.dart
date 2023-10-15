82f9bd6271fedbda5c2845a344244cdb3a3c14e5
Nader <nsyd1582@gmail.com>
2023-10-15 04:23:28 +0300

    create custom socialmedia row

--- lib/features/authentication/presentation/views/widgets/Login widgets/login_view_body.dart
+++ lib/features/authentication/presentation/views/widgets/Login widgets/login_view_body.dart
@@ -11,6 +11,7 @@ import 'package:nectar/core/widgets/custom_button.dart';
 import '../../../../../../core/widgets/custom_toast_text.dart';
 import '../../../manager/cubit/login_cubit.dart';
 import 'custom_checkbox_row.dart';
+import 'custom_socialmedia_button.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -187,50 +188,6 @@ class CustomHaveAccountText extends StatelessWidget {
                   .copyWith(color: AppColors.oceanGreen),
               recognizer: TapGestureRecognizer()..onTap = () {})
         ]));
-  }
-}
-
-class CustomSocialMediaRow extends StatelessWidget {
-  const CustomSocialMediaRow({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        CustomSocialMediaButton(),
-        CustomSocialMediaButton(),
-        /*Container(
-          padding: const EdgeInsets.all(5),
-          width: MediaQuery.of(context).size.width / 2 - 40,
-          height: 50,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: AppColors.grey)),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Image.asset(
-                  'assets/images/facebook.png',
-                  color: AppColors.facebookColor,
-                  width: 40,
-                  height: 40,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              const SizedBox(
-                width: 30,
-              ),
-              const Text('Facebook'),
-            ],
-          ),
-        )*/
-      ],
-    );
   }
 }
 
