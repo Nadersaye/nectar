9642c44f18042d1c201a4fcec8cfe18fa77bcf20
Nader <nsyd1582@gmail.com>
2023-10-15 04:19:43 +0300

    create custom socialmrdia button

--- lib/features/authentication/presentation/views/widgets/Login widgets/login_view_body.dart
+++ lib/features/authentication/presentation/views/widgets/Login widgets/login_view_body.dart
@@ -234,38 +234,6 @@ class CustomSocialMediaRow extends StatelessWidget {
   }
 }
 
-class CustomSocialMediaButton extends StatelessWidget {
-  const CustomSocialMediaButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(5),
-      width: MediaQuery.of(context).size.width / 2 - 40,
-      height: 50,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: AppColors.grey)),
-      child: Row(
-        children: [
-          Image.asset(
-            'assets/images/google.png',
-            width: 40,
-            height: 40,
-            fit: BoxFit.fill,
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          const Text('Google'),
-        ],
-      ),
-    );
-  }
-}
-
 class CustomSeperatorRow extends StatelessWidget {
   const CustomSeperatorRow({
     super.key,
