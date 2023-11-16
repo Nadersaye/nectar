a56f1eeeb342d6b12e0ade2a767ed83bada30ca2
Nader <ns1568@fayoum.edu.eg>
2023-11-16 16:31:00 +0200

    add login google states

---
+++ lib/features/authentication/presentation/manager/login google cubit/login_google_state.dart
@@ -0,0 +1,16 @@
+part of 'login_google_cubit.dart';
+
+@immutable
+sealed class LoginGoogleState {}
+
+final class LoginGoogleInitial extends LoginGoogleState {}
+
+final class LoginGoogleLoading extends LoginGoogleState {}
+
+final class LoginGoogleSuccess extends LoginGoogleState {}
+
+final class LoginGoogleFailure extends LoginGoogleState {
+  final String errorMessage;
+
+  LoginGoogleFailure({required this.errorMessage});
+}
