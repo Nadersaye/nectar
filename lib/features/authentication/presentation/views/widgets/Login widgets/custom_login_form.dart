868b51002db07422f3a9b63a510e7dca0feeac1f
Nader <ns1568@fayoum.edu.eg>
2023-11-19 11:59:53 +0200

    use validate login  &login cubit &bloc listener

--- lib/features/authentication/presentation/views/widgets/Login widgets/custom_login_form.dart
+++ lib/features/authentication/presentation/views/widgets/Login widgets/custom_login_form.dart
@@ -1,5 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:nectar/features/authentication/presentation/manager/validate%20login/validate_login_cubit.dart';
+import '../../../../../../core/utils/app_routes.dart';
 import '../../../../../../core/widgets/custom_button.dart';
+import '../../../manager/login cubit/login_cubit.dart';
+import '../../../manager/reset password cubit/reset_password_cubit.dart';
 import 'custom_checkbox_row.dart';
 import 'input_login_details.dart';
 
@@ -14,6 +22,25 @@ class CustomLoginForm extends StatefulWidget {
 class _CustomLoginFormState extends State<CustomLoginForm> {
   GlobalKey<FormState> formkey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  bool isPassword = true;
+  bool isLogin = true;
+  Timer? _timer;
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -26,40 +53,72 @@ class _CustomLoginFormState extends State<CustomLoginForm> {
             padding: const EdgeInsets.symmetric(horizontal: 25),
             child: InputLoginDetails(
               autovalidateMode: autovalidateMode,
+              emailController: emailController,
+              passwordController: passwordController,
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(15, 0, 25, 40),
-            child: CustomCheckBoxRow(),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Center(
-              child: CustomActionButton(
-                buttonText: 'Login',
-                onTap: () {
-                  if (formkey.currentState!.validate()) {
-                  } else {
+            padding: const EdgeInsets.fromLTRB(15, 0, 25, 40),
+            child: CustomCheckBoxRow(
+              emailController: emailController,
+              onTap: () {
+                BlocProvider.of<ValidateLoginCubit>(context).changeToReset();
+                if (formkey.currentState!.validate()) {
+                  BlocProvider.of<ResetPasswordCubit>(context).email =
+                      emailController.text;
+                  BlocProvider.of<ResetPasswordCubit>(context).resetPassword();
+                } else {
+                  setState(() {
                     autovalidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
-              ),
+                  });
+                }
+              },
             ),
+          ),
+          BlocConsumer<LoginCubit, LoginState>(
+            listener: (context, state) async {
+              if (state is LoginFailure) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(state.errorMessage)));
+                _timer?.cancel();
+                await EasyLoading.dismiss();
+              } else if (state is LoginSuccess) {
+                _timer?.cancel();
+                await EasyLoading.showSuccess('Great Success!');
+                AppRoutes.router.replace(AppRoutes.homeView);
+              } else {
+                _timer?.cancel();
+                await EasyLoading.show(
+                  status: 'loading...',
+                  maskType: EasyLoadingMaskType.black,
+                );
+              }
+            },
+            builder: (context, state) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Center(
+                  child: CustomActionButton(
+                    buttonText: 'Login',
+                    onTap: () {
+                      BlocProvider.of<ValidateLoginCubit>(context)
+                          .changeToLogin();
+                      if (formkey.currentState!.validate()) {
+                        BlocProvider.of<LoginCubit>(context)
+                            .validateLogin(emailController, passwordController);
+                      } else {
+                        setState(() {
+                          autovalidateMode = AutovalidateMode.always;
+                        });
+                      }
+                    },
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
     );
   }
 }
-/*
-
-        if (state is LoginSuccesState) {
-          customToastText(
-              toastMessage: 'successful logged', state: ToastStates.success);
-        } else if (state is LoginErrorState) {
-          customToastText(
-              toastMessage: 'please enter correct data',
-              state: ToastStates.error);
-        }
- */
