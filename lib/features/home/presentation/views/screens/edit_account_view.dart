60ee314062fabef57029a4836894253c188e97fb
Nader <ns1568@fayoum.edu.eg>
2023-11-19 14:34:35 +0200

    create edit account view

---
+++ lib/features/home/presentation/views/screens/edit_account_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/edit account widgets/edit_account_view_body.dart';
+
+class EditAccountView extends StatelessWidget {
+  const EditAccountView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: EditAccountViewBody(),
+    );
+  }
+}
