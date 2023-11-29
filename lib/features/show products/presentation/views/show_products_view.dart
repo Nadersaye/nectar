30595a154a239e1ac046611ef847c51ec78ebcd4
Nader <ns1568@fayoum.edu.eg>
2023-11-29 23:37:52 +0200

    use CartI tem Model

--- lib/features/show products/presentation/views/show_products_view.dart
+++ lib/features/show products/presentation/views/show_products_view.dart
@@ -8,35 +8,14 @@ import 'widgets/show_products_view_body.dart';
 class ShowProductsView extends StatefulWidget {
   const ShowProductsView({
     super.key,
+    required this.cartItems,
   });
-
+  final List<CartItemModel> cartItems;
   @override
   State<ShowProductsView> createState() => _ShowProductsViewState();
 }
 
 class _ShowProductsViewState extends State<ShowProductsView> {
-  List<CartItemModel> productItems = [
-    CartItemModel(
-        7.35, 7, 'assets/images/apple.png', 'apple', '2 kg ,price', true, 0, 0),
-    CartItemModel(2.35, 2, 'assets/images/apple2.png', 'apple', '.5 kg ,price',
-        true, 0, 0),
-    CartItemModel(3.35, 3, 'assets/images/apple3.png', 'apple', '1 kg ,price',
-        true, 0, 0),
-    CartItemModel(
-        2.35, 2, 'assets/images/apple.png', 'apple', '3 kg ,price', true, 0, 0),
-    CartItemModel(2.35, 2, 'assets/images/apple2.png', 'apple', '1 kg ,price',
-        true, 0, 0),
-    CartItemModel(12.75, 11, 'assets/images/apple3.png', 'apple', '4 kg ,price',
-        true, 0, 0),
-    CartItemModel(
-        2.35, 2, 'assets/images/apple.png', 'apple', '1 kg ,price', true, 0, 0),
-    CartItemModel(15.35, 13, 'assets/images/apple.png', 'apple', '5 kg ,price',
-        true, 0, 0),
-    CartItemModel(
-        2.35, 2, 'assets/images/apple.png', 'apple', '1 kg ,price', true, 0, 0),
-    CartItemModel(30.35, 25, 'assets/images/apple2.png', 'apple', '7 kg ,price',
-        true, 0, 0),
-  ];
   //final List<CartItemModel> productsItems;
   @override
   Widget build(BuildContext context) {
@@ -56,7 +35,7 @@ class _ShowProductsViewState extends State<ShowProductsView> {
         body: Padding(
           padding: const EdgeInsets.all(25.0),
           child: ShowProductsViewBody(
-            productsItems: productItems,
+            productsItems: widget.cartItems,
           ),
         ),
       ),
