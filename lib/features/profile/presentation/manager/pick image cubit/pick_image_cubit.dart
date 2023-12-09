06a279c5ac1aa97b80efdb9035a087dd8c7c8b31
Nader <ns1568@fayoum.edu.eg>
2023-12-10 00:01:06 +0200

    add check null for the image and test print it

--- lib/features/profile/presentation/manager/pick image cubit/pick_image_cubit.dart
+++ lib/features/profile/presentation/manager/pick image cubit/pick_image_cubit.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
@@ -12,13 +13,22 @@ class PickImageCubit extends Cubit<PickImageState> {
   File? file;
   String? baseName;
   pickImage() async {
+    emit(PickImageLoading());
+    debugPrint(
+        '//////////////////////////////pick loading///////////////////////////////////////////');
     var responce = await imageRepo.pickImage();
     responce.fold((error) {
+      debugPrint(
+          '//////////////////////////////pick error///////////////////////////////////////////');
       emit(PickImageFailure(errorMessage: error));
     }, (image) {
-      file = File(image.path);
-      baseName = p.basename(image.path);
-      emit(PickImageSuccess());
+      if (image != null) {
+        debugPrint(
+            '//////////////////////////////pick success///////////////////////////////////////////');
+        file = File(image.path);
+        baseName = p.basename(image.path);
+        emit(PickImageSuccess());
+      }
     });
   }
 }
