b637595c9183ef92cd59e122e6f9f74228cf5a2b
Nader <ns1568@fayoum.edu.eg>
2023-12-31 07:26:21 +0200

    refactor code

--- lib/features/home/presentation/manager/explore categories cubit/explore_categories_products_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-part 'explore_categories_products_state.dart';
-
-class ExploreCategoriesProductsCubit
-    extends Cubit<ExploreCategoriesProductsState> {
-  ExploreCategoriesProductsCubit() : super(ExploreCategoriesProductsInitial());
-}
--- lib/features/home/presentation/manager/explore categories cubit/explore_categories_products_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'explore_categories_products_cubit.dart';
-
-@immutable
-sealed class ExploreCategoriesProductsState {}
-
-final class ExploreCategoriesProductsInitial
-    extends ExploreCategoriesProductsState {}
