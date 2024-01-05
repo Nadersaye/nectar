fce79a273d26be9e32dd855761e4bf0e57c3e619
Nader <ns1568@fayoum.edu.eg>
2024-01-05 05:35:36 +0200

    use shared Preferences to store customer id

--- lib/features/authentication/presentation/views/widgets/Login widgets/custom_login_form.dart
+++ lib/features/authentication/presentation/views/widgets/Login widgets/custom_login_form.dart
@@ -1,15 +1,15 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:nectar/features/authentication/presentation/manager/validate%20login/validate_login_cubit.dart';
 import '../../../../../../core/utils/app_routes.dart';
 import '../../../../../../core/widgets/custom_button.dart';
 import '../../../manager/login cubit/login_cubit.dart';
 import '../../../manager/reset password cubit/reset_password_cubit.dart';
+import '../../../manager/validate login/validate_login_cubit.dart';
 import 'custom_checkbox_row.dart';
 import 'input_login_details.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CustomLoginForm extends StatefulWidget {
   const CustomLoginForm({
@@ -83,9 +83,13 @@ class _CustomLoginFormState extends State<CustomLoginForm> {
                 _timer?.cancel();
                 await EasyLoading.dismiss();
               } else if (state is LoginSuccess) {
+                SharedPreferences sharedPreferences =
+                    await SharedPreferences.getInstance();
+                sharedPreferences.setString("id", state.user.uid);
                 _timer?.cancel();
                 await EasyLoading.showSuccess('Great Success!');
-                AppRoutes.router.replace(AppRoutes.homeView);
+                AppRoutes.router
+                    .pushReplacement(AppRoutes.homeView, extra: state.user.uid);
               } else {
                 _timer?.cancel();
                 await EasyLoading.show(
