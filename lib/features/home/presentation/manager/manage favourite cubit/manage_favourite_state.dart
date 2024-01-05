69f9a5d0932ca8b12a4381f0616b6b9485e73dd4
Nader <ns1568@fayoum.edu.eg>
2024-01-05 04:21:21 +0200

    add loading & success & failure states

--- lib/features/home/presentation/manager/manage favourite cubit/manage_favourite_state.dart
+++ lib/features/home/presentation/manager/manage favourite cubit/manage_favourite_state.dart
@@ -5,4 +5,14 @@ sealed class ManageFavouriteState {}
 
 final class ManageFavouriteInitial extends ManageFavouriteState {}
 
+final class ManageFavouriteLoading extends ManageFavouriteState {}
+
+final class ManageFavouriteSuccess extends ManageFavouriteState {}
+
+final class ManageFavouriteFailure extends ManageFavouriteState {
+  final String errorMessage;
+
+  ManageFavouriteFailure({required this.errorMessage});
+}
+
 final class ManageFavouriteChanged extends ManageFavouriteState {}
