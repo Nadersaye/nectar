44c13229b6eefc5016c32137982dfdb5284bded8
Nader <ns1568@fayoum.edu.eg>
2024-01-05 04:35:52 +0200

    add favourite products view body

--- lib/features/home/presentation/views/widgets/favourite products widgets/favourite_products_view_body.dart
+++ lib/features/home/presentation/views/widgets/favourite products widgets/favourite_products_view_body.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:nectar/core/utils/colors.dart';
 import 'package:nectar/core/utils/styles.dart';
-import '../../../../../../core/models/cart_item_model.dart';
 import 'favourite_list_view.dart';
 
 class FavouriteProductsViewBody extends StatelessWidget {
-  const FavouriteProductsViewBody({super.key, required this.cartItems});
-  final List<CartItemModel> cartItems;
+  const FavouriteProductsViewBody({super.key});
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -14,19 +13,18 @@ class FavouriteProductsViewBody extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(top: 30, bottom: 30),
           child: Text(
-            'My Cart',
+            'My Favourites',
             style: Styles.styleTimberGreen20
                 .copyWith(color: AppColors.blackRussian),
           ),
         ),
         const Divider(
           height: 1,
-          color: AppColors.lightGray,
+          color: AppColors.grey,
         ),
-        Expanded(
-          child: FavouriteProductsListView(
-            productItems: cartItems,
-          ),
+        const Gap(7.5),
+        const Expanded(
+          child: FavouriteProductsListView(),
         ),
       ],
     );
