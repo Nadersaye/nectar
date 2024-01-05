df37b5b0eb2c84f026d3bb0a2f8cfa40f1b14156
Nader <ns1568@fayoum.edu.eg>
2024-01-05 05:20:37 +0200

    modify custom product details data

--- lib/features/home/presentation/views/widgets/product details widgets/custom_product_details_data.dart
+++ lib/features/home/presentation/views/widgets/product details widgets/custom_product_details_data.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nectar/core/models/cart_item_model.dart';
+import 'package:nectar/core/models/product_item_model.dart';
+import 'package:nectar/features/home/presentation/manager/add%20cart%20cubit/add_cart_cubit.dart';
+import 'package:nectar/features/home/presentation/manager/control%20count%20cubit/control_count_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../../../../../core/widgets/custom_button.dart';
 import 'custom_control_product_count.dart';
 import 'custom_nutritions_tile.dart';
 import 'custom_product_detail_tile.dart';
+import 'custom_product_details_content.dart';
 import 'custom_review_tile.dart';
-import 'product_details_body.dart';
 
 class CustomProductDetailsData extends StatelessWidget {
-  const CustomProductDetailsData({
-    super.key,
-    required this.widget,
-  });
+  const CustomProductDetailsData({super.key, required this.product});
 
-  final ProductDetailsBody widget;
+  final ProductItemModel product;
 
   @override
   Widget build(BuildContext context) {
@@ -20,19 +24,41 @@ class CustomProductDetailsData extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
       child: SizedBox(
         width: double.infinity,
-        child: CustomProductDetailsDataContent(widget: widget),
+        child: CustomProductDetailsDataContent(
+          product: product,
+        ),
       ),
     );
   }
 }
 
-class CustomProductDetailsDataContent extends StatelessWidget {
+class CustomProductDetailsDataContent extends StatefulWidget {
   const CustomProductDetailsDataContent({
     super.key,
-    required this.widget,
+    required this.product,
   });
 
-  final ProductDetailsBody widget;
+  final ProductItemModel product;
+
+  @override
+  State<CustomProductDetailsDataContent> createState() =>
+      _CustomProductDetailsDataContentState();
+}
+
+class _CustomProductDetailsDataContentState
+    extends State<CustomProductDetailsDataContent> {
+  String? userId;
+  getUser() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String id = sharedPreferences.getString("id")!;
+    userId = id;
+  }
+
+  @override
+  void initState() {
+    getUser();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,8 +66,11 @@ class CustomProductDetailsDataContent extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        ProductDetailsContent(
+          product: widget.product,
+        ),
         const SizedBox(
-          height: 30,
+          height: 15,
         ),
         CustomControlProductCount(
           product: widget.product,
@@ -49,14 +78,47 @@ class CustomProductDetailsDataContent extends StatelessWidget {
         const SizedBox(
           height: 30,
         ),
-        CustomProductDetailTile(widget: widget),
-        CustomNutritionsTile(widget: widget),
-        CustomReviewTile(widget: widget),
+        CustomProductDetailTile(product: widget.product),
+        CustomNutritionsTile(product: widget.product),
+        CustomReviewTile(product: widget.product),
         const SizedBox(
           height: 10,
         ),
-        const CustomActionButton(
-          buttonText: 'Add To Basket',
+        BlocBuilder<ControlCountCubit, ControlCountState>(
+          builder: (context, state) {
+            return AnimatedOpacity(
+              opacity: BlocProvider.of<ControlCountCubit>(context).count > 0
+                  ? 1
+                  : .2,
+              duration: const Duration(milliseconds: 300),
+              child: CustomActionButton(
+                buttonText: 'Add To Basket',
+                onTap: () async {
+                  if (userId != null &&
+                      BlocProvider.of<ControlCountCubit>(context).count > 0) {
+                    await BlocProvider.of<AddCartCubit>(context).addCart(
+                        CartItemModel(
+                            userId: userId!,
+                            productId: widget.product.id!,
+                            price: widget.product.offerPrice > 0
+                                ? widget.product.offerPrice
+                                : widget.product.price,
+                            imageUrl: widget.product.imageUrl ??
+                                "https://demofree.sirv.com/nope-not-here.jpg",
+                            title: widget.product.title,
+                            count: BlocProvider.of<ControlCountCubit>(context)
+                                .count,
+                            totalPrice:
+                                BlocProvider.of<ControlCountCubit>(context)
+                                    .total,
+                            size: widget.product.size,
+                            type: widget.product.type));
+                    GoRouter.of(context).pop();
+                  }
+                },
+              ),
+            );
+          },
         )
       ],
     );
