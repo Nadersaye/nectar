b12dd8c83c4cd81d7c0a3b720fa92d5d6fb0bd53
Nader <ns1568@fayoum.edu.eg>
2024-01-05 04:28:02 +0200

    edit manage favourite function and fix issues

--- lib/features/home/presentation/manager/manage favourite cubit/manage_favourite_cubit.dart
+++ lib/features/home/presentation/manager/manage favourite cubit/manage_favourite_cubit.dart
@@ -1,29 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../../core/utils/colors.dart';
+import 'package:nectar/core/utils/constants.dart';
 part 'manage_favourite_state.dart';
 
 class ManageFavouriteCubit extends Cubit<ManageFavouriteState> {
   ManageFavouriteCubit() : super(ManageFavouriteInitial());
   static ManageFavouriteCubit get(context) => BlocProvider.of(context);
-  IconData? icon;
-  Color? color;
-  bool changedIcon = false;
-  manageIconAndColor() {
-    icon = changeIcon();
-    color = changeIconColor();
-  }
-
-  changeFavourite() {
-    changedIcon = !changedIcon;
+  bool? isFavorite;
+  _changeFavourite() {
+    isFavorite = !isFavorite!;
     emit(ManageFavouriteChanged());
   }
 
-  IconData changeIcon() {
-    return changedIcon ? Icons.favorite : Icons.favorite_border_outlined;
-  }
+  void toggleFavoriteStatus({required String productId}) async {
+    // Retrieve the user's ID from Firebase Authentication
+    try {
+      ManageFavouriteLoading();
+      final User? user = FirebaseAuth.instance.currentUser;
 
-  Color changeIconColor() {
-    return changedIcon ? Colors.red : AppColors.grey;
+      if (user != null) {
+        // Check if the product is already a favorite
+        if (isFavorite == true) {
+          // Remove the product from the user's favorites in Firestore
+          final QuerySnapshot snapshot = await FirebaseFirestore.instance
+              .collection(favoriteCollection)
+              .where('userId', isEqualTo: user.uid)
+              .where('productId', isEqualTo: productId)
+              .get();
+
+          final docId = snapshot.docs.first.id;
+          await FirebaseFirestore.instance
+              .collection(favoriteCollection)
+              .doc(docId)
+              .delete();
+        } else {
+          // Add the product to the user's favorites in Firestore
+          await FirebaseFirestore.instance.collection('favorites').add({
+            'userId': user.uid,
+            'productId': productId,
+          });
+        }
+        // Update the favorite status and icon color
+        _changeFavourite();
+        emit(ManageFavouriteSuccess());
+      }
+    } catch (e) {
+      emit(ManageFavouriteFailure(errorMessage: e.toString()));
+    }
   }
 }
