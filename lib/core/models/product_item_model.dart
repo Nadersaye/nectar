e792f6a6809aaff39ce2d5703cd662f376bd4eb7
Nader <ns1568@fayoum.edu.eg>
2024-01-05 04:08:44 +0200

    edit product item model

--- lib/core/models/product_item_model.dart
+++ lib/core/models/product_item_model.dart
@@ -3,44 +3,44 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ProductItemModel {
   final String? id;
   final String title;
-  final String subtitle;
   final String type; //
   final String category; //
   final String company; //
-  final String imageUrl; //
+  final String? imageUrl; //
   final num rate; //
-  final String benefits; //
-  final String details; //
+  final String? benefits; //
+  final String? details; //
   final num price; //
   final num offerPrice; //
   final num? percent;
+  final num size;
 
   ProductItemModel(
       {this.id,
       required this.title,
-      required this.subtitle,
       required this.type,
       required this.category,
       required this.company,
       required this.imageUrl,
       required this.rate,
-      required this.benefits,
-      required this.details,
+      this.benefits,
+      this.details,
       required this.price,
       required this.offerPrice,
-      this.percent});
+      this.percent,
+      required this.size});
   toJson() => {
         "type": type,
         "category": category,
         "company": company,
         "imageUrl": imageUrl,
-        "rate": rate,
+        "rate": rate as double,
         "benefits": benefits,
         "details": details,
         "price": price,
         "offerPrice": offerPrice,
         "title": title,
-        "subtitle": subtitle
+        "size": size
       };
   factory ProductItemModel.fromSnapShot(
       DocumentSnapshot<Map<String, dynamic>> document) {
@@ -48,16 +48,16 @@ class ProductItemModel {
     return ProductItemModel(
       id: document.id,
       title: data['title'],
-      subtitle: data['subtitle'],
       type: data['type'],
       category: data['category'],
       company: data['company'],
       imageUrl: data['imageUrl'],
-      rate: data['rate'],
+      rate: data['rate'] as double,
       benefits: data['benefits'],
       details: data['details'],
       price: data['price'],
       offerPrice: data['offerPrice'],
+      size: data['size'],
       percent: ((data['price'] - data['offerPrice']) / data['price'] * 100),
     );
   }
