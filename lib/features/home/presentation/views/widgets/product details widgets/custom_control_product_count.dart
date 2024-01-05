cef676fa9e25c41cc5a82a8e94402a7f2d1b4e7e
Nader <ns1568@fayoum.edu.eg>
2024-01-05 05:19:22 +0200

    modify custom product count

--- lib/features/home/presentation/views/widgets/product details widgets/custom_control_product_count.dart
+++ lib/features/home/presentation/views/widgets/product details widgets/custom_control_product_count.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nectar/core/models/product_item_model.dart';
 import '../../../../../../core/utils/colors.dart';
 import '../../../../../../core/utils/styles.dart';
+import '../../../manager/control count cubit/control_count_cubit.dart';
 import 'custom_appbar_icon.dart';
 import 'custom_product_price_widget.dart';
 import '../../../../../../core/widgets/custom_product_total_price_widget.dart';
 
-class CustomControlProductCount extends StatelessWidget {
+class CustomControlProductCount extends StatefulWidget {
   const CustomControlProductCount({
     super.key,
     required this.product,
   });
   final ProductItemModel product;
+
+  @override
+  State<CustomControlProductCount> createState() =>
+      _CustomControlProductCountState();
+}
+
+class _CustomControlProductCountState extends State<CustomControlProductCount> {
   @override
   Widget build(BuildContext context) {
+    var get = ControlCountCubit.get(context);
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -26,17 +36,27 @@ class CustomControlProductCount extends StatelessWidget {
           child: AspectRatio(
             aspectRatio: 1 / 1,
             child: CustomIcon(
-                icon: const Icon(Icons.remove), size: 23, onPressed: () {}),
+                icon: const Icon(Icons.remove),
+                size: 23,
+                onPressed: () {
+                  get.decrementCount(
+                      offerPrice: widget.product.offerPrice,
+                      price: widget.product.price);
+                }),
           ),
         ),
         const SizedBox(
           width: 10,
         ),
-        Center(
-          child: Text(
-            '',
-            style: Styles.styleBlackRussian18,
-          ),
+        BlocBuilder<ControlCountCubit, ControlCountState>(
+          builder: (context, state) {
+            return Center(
+              child: Text(
+                get.count.toString(),
+                style: Styles.styleBlackRussian18,
+              ),
+            );
+          },
         ),
         const SizedBox(
           width: 10,
@@ -54,20 +74,40 @@ class CustomControlProductCount extends StatelessWidget {
                   color: AppColors.oceanGreen,
                   size: 23,
                 ),
-                onPressed: () {}),
+                onPressed: () {
+                  get.incrementCount(
+                      offerPrice: widget.product.offerPrice,
+                      price: widget.product.price);
+                }),
           ),
         ),
         const Spacer(),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomProductPrice(
-              product: product,
-            ),
-            const ProductTotalPrice(), /////////////////////
-          ],
-        )
+        CustomCartPrice(product: widget.product)
+      ],
+    );
+  }
+}
+
+class CustomCartPrice extends StatelessWidget {
+  const CustomCartPrice({
+    super.key,
+    required this.product,
+  });
+
+  final ProductItemModel product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomProductPrice(
+          product: product,
+        ),
+        ProductTotalPrice(
+          product: product,
+        ),
       ],
     );
   }
